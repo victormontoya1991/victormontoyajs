b8cd1e83c1bdce2735ec48de7e17d5c55d4dbff0
victormontoya1991 <disvictormontoya@gmail.com>
2022-11-11 09:32:17 -0500

    carga

--- js/data.js
+++ js/data.js
@@ -1,16 +1,17 @@
 class Motorcycles{ 
-    constructor(brand, model, years, state, setting, preci, sku){
-        this.brand = brand;
+    constructor(model, preci, sku){
         this.model = model;
-        this.years = years;
-        this.state = state;
-        this.setting = setting;
         this.preci = preci;
         this.sku= sku;
     }
 }
-const pulsar125cc = new Motorcycles("Pulsar","125cc","2021","Nueva","Sencilla",8000000,"MT0001");
-const pulsar175cc = new Motorcycles("Pulsar","175cc","2022","Nueva","ABS",10000000,"MT0004");
-const pulsar200cc = new Motorcycles("Pulsar","200cc","2021","Nueva","Sencilla",12000000,"MT0005");
+const benelli150cc = new Motorcycles();
+const benelli250cc = new Motorcycles();
+const benelli752cc = new Motorcycles();
 
-console.log ( pulsar125cc, pulsar175cc, pulsar200cc )
+console.log ( benelli150cc, benelli250cc, benelli752cc )
+
+
+class Products{
+
+}
