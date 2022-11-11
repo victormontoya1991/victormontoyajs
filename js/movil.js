b45cb7cf8f3e4524868e8ef6a6e12f70b901d145
victormontoya1991 <disvictormontoya@gmail.com>
2022-11-11 15:17:24 -0500

    carga SCSS

---
+++ js/movil.js
@@ -0,0 +1,30 @@
+(function(){
+    const propMenu = {
+        burger_menu: document.getElementById('iconMenu'),
+        slideMenu: document.getElementById('slideMenu'),
+        menu_activo: false,
+        elem_menu: document.querySelectorAll('#slideMenu .menuPrincipal a')
+    }
+    const metMenu = {	
+        inicio: function () {		
+            propMenu.burger_menu.addEventListener('click', metMenu.toggleMenu);
+            for (var i = 0; i < propMenu.elem_menu.length; i++) {
+                propMenu.elem_menu[i].addEventListener('click', metMenu.ocultarMenu);
+            }
+        },
+        toggleMenu: function () {
+            if ( propMenu.menu_activo == false ) {
+                propMenu.menu_activo = true;
+                propMenu.slideMenu.className = propMenu.slideMenu.className + ' active';
+            } else{
+                propMenu.menu_activo = false;
+                propMenu.slideMenu.className = propMenu.slideMenu.className.replace('active', '');
+            }
+        },
+        ocultarMenu: function () {
+            propMenu.menu_activo = false;
+            propMenu.slideMenu.className = propMenu.slideMenu.className.replace('active', '');
+        }
+    }
+    metMenu.inicio()
+}())
---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let menu = document.getElementById("iconMenu");
+function myFunction(x) {
+    menu.classList.toggle("change-menu");
+    x.classList.toggle("change");
+}
