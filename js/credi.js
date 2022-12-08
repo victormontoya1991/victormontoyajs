9373fe61b7e09832de8e93544a91ffe12221b829
victormontoya1991 <disvictormontoya@gmail.com>
2022-12-08 18:50:35 -0500

    carga

---
+++ js/credi.js
@@ -0,0 +1,9 @@
+// Declaraciones
+//--------------
+// Array
+//------
+const dataMotorcycles = [benelli150cc, benelli180cc, benelli251cc,benelli302cc,benelli500cc]
+const carMontobiker = JSON.parse(localStorage.getItem('carMontobiker')) || []
+
+// Query de Elementos
+const crediMotorbike = document.querySelector('#crediMotorbike')
