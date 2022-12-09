0c3d2d611fc8dea860efe7575e14362bd225441c
victormontoya1991 <disvictormontoya@gmail.com>
2022-12-09 00:16:11 -0500

    arreglo calculo credito

--- js/credi.js
+++ js/credi.js
@@ -14,21 +14,25 @@ const crediMotorbike = document.querySelector('#crediMotorbike')
 
 const carBikers = () => {
     carMontobiker.forEach((motorbiker)=>{
-        crediMotorbike.innerHTML=`
-        <div class="carBikers">
+        const credibike = document.createElement('a')
+        credibike.classList.add('carBikers')
+        credibike.setAttribute('data-sku', motorbiker.sku)
+        credibike.innerHTML=`
             <div class="imgCarbikers">
                 <img src="${motorbiker.img}">
             </div>
             <div class="infCarbikers">
                 <h5>${motorbiker.name}</h5>
-                <h4>$${motorbiker.preci}</h4>
+                <p>SKU: ${motorbiker.sku}</p>
+                <h4>Precio: $${motorbiker.preci}</h4>
             </div>
-            <a class="iconCarbikers">
-                <svg class="cardIcon" xmlns="http://www.w3.org/2000/svg" id="Isolation_Mode" data-name="Isolation Mode" viewBox="0 0 24 24" width="50" height="50" fill="#373736"><path d="M8.127,24l9.507-9.52a3.507,3.507,0,0,0,0-4.948L8.116,0,6,2.121l9.518,9.531a.5.5,0,0,1,0,.707L6.01,21.879Z"/></svg>
-            </a>
-        </div>
+            <div class="iconCarbikers">
+                <svg x="0px" y="0px" width="50" height="50" viewBox="0 0 50 50"> <path d="M 9.15625 6.3125 L 6.3125 9.15625 L 22.15625 25 L 6.21875 40.96875 L 9.03125 43.78125 L 25 27.84375 L 40.9375 43.78125 L 43.78125 40.9375 L 27.84375 25 L 43.6875 9.15625 L 40.84375 6.3125 L 25 22.15625 Z"></path></svg>
+            </div>
         `
+        crediMotorbike.append(credibike)
     })
+
 }
 
 /*Rederizado aviso*/
